457e97f70d7d4b0de26ee1dcc060d9c30da9d4a8
camenginx <cameng.adisaputra@inxsoftware.com>
2025-01-26 10:35:58 +0800

    update cl bat file.

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,4 +1,8 @@
 @echo off
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
+  call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else (
+  call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
 set path=D:\datapersonal\Projects\handmadehero\misc;%path%
